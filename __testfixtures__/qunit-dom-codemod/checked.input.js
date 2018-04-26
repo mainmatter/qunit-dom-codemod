8c02065cb45fb50adab141bcb98097d0fe4bfb1a
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-04-26 16:27:07 +0200

    Add support for `assert.ok(find('foo:checked'))`

--- __testfixtures__/qunit-dom-codemod/checked.input.js
+++ __testfixtures__/qunit-dom-codemod/checked.input.js
@@ -8,5 +8,8 @@ assert.equal(find('.foo').checked, false);
 assert.ok(find('input[type=checkbox]', formDiv).is(':checked'), 'There is value in checkbox');
 assert.equal(find('input[type=checkbox]')[0].is(':checked'), false);
 
+assert.ok(find('.checkbox-appointments input:checked'), 'Appointments checkbox is checked');
+assert.notOk(find('.checkbox-appointments input:checked'), 'Appointments checkbox is not checked');
+
 assert.equal(find('.checkbox-appointments input:checked').length, 0, 'Appointments checkbox is not checked');
 assert.equal(find('.checkbox-appointments input:checked').length, 1, 'Appointments checkbox is checked');
--- qunit-dom-codemod.js
+++ qunit-dom-codemod.js
@@ -120,6 +120,42 @@ export default function(file, api, options) {
     return j(`x({ count: ${count} })`).find(j.ObjectExpression).paths()[0].node;
   }
 
+  // assert.ok(find('input:checked')) -> assert.dom('input:checked').isChecked()
+  // assert.notOk(find('input:checked')) -> assert.dom('input:checked').isNotChecked()
+
+  root.find(j.CallExpression, assertBoolean)
+    .filter(p => j.match(p.get('arguments').get('0'), {
+      type: 'CallExpression',
+      callee: { name: 'find' },
+    }) || j.match(p.get('arguments').get('0'), {
+      type: 'MemberExpression',
+      object: {
+        type: 'CallExpression',
+        callee: { name: 'find' },
+      },
+      property: {
+        type: 'Literal',
+        value: 0,
+      },
+    }))
+    .forEach(p => {
+      let findNode = p.node.arguments[0];
+      if (findNode.type === 'MemberExpression') {
+        findNode = findNode.object;
+      }
+
+      let selector = findNode.arguments[0];
+      if (isSelector(selector) && selector.value.endsWith(':checked')) {
+        let customMessage = getCustomMessageOfTruthyAssertion(p);
+        let assertion = isTruthyAssertion(p) ? 'isChecked' : 'isNotChecked';
+
+        selector.value = selector.value.substr(0, selector.value.length - 8);
+
+        p.replace(domAssertion(findNode.arguments, assertion, customMessage ? [customMessage] : []));
+      }
+    });
+
+
   // assert.equal(find('input:checked').length, 1) -> assert.dom('input:checked').isChecked()
   // assert.equal(find('input:checked').length, 0) -> assert.dom('input:checked').isNotChecked()
 
